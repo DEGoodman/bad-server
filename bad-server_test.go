eef971ef32d15a04a043917345091a802928f270
Erik Goodman <erik.goodman@nextiva.com>
2020-02-13 16:43:44 -0700

    add alt test

--- bad-server_test.go
+++ bad-server_test.go
@@ -53,3 +53,23 @@ func TestBaseResponse(t *testing.T) {
 			rr.Body.String(), expected)
 	}
 }
+
+func TestAltNameResponse(t *testing.T) {
+	req, err := http.NewRequest("GET", "/?name=Erik", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(rootHandler)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	// check the response is what we expect.
+	expected := "Hello, Erik\n"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
